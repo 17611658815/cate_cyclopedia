9bd772a938ab6116d4d0f50a6870847dcf06be9b
17611658815 <17611658815@163.com>
2019-05-13 13:51:25 +0800

    2019-5-13

--- pages/index/index.js
+++ pages/index/index.js
@@ -42,52 +42,52 @@ Page({
         })
 
     },
-    onImageLoad: function(e) {
-        let imageId = e.currentTarget.id;
-        let oImgW = e.detail.width; //图片原始宽度
-        let oImgH = e.detail.height; //图片原始高度
-        let imgWidth = this.data.imgWidth; //图片设置的宽度
-        let scale = imgWidth / oImgW; /*比例计算*/
-        let imgHeight = oImgH * scale; //自适应高度
-        let images = this.data.images;
-        let imageObj = null;
+    // onImageLoad: function(e) {
+    //     let imageId = e.currentTarget.id;
+    //     let oImgW = e.detail.width; //图片原始宽度
+    //     let oImgH = e.detail.height; //图片原始高度
+    //     let imgWidth = this.data.imgWidth; //图片设置的宽度
+    //     let scale = imgWidth / oImgW; /*比例计算*/
+    //     let imgHeight = oImgH * scale; //自适应高度
+    //     let images = this.data.images;
+    //     let imageObj = null;
 
-        for (let i = 0; i < images.length; i++) {
-            let img = images[i];
-            if (img.recipe.id === imageId) {
-                imageObj = img;
-                break;
-            }
-        }
-        imageObj.height = imgHeight;
+    //     for (let i = 0; i < images.length; i++) {
+    //         let img = images[i];
+    //         if (img.recipe.id === imageId) {
+    //             imageObj = img;
+    //             break;
+    //         }
+    //     }
+    //     imageObj.height = imgHeight;
 
-        let loadingCount = this.data.loadingCount - 1;
-        let col1 = this.data.col1;
-        let col2 = this.data.col2;
+    //     let loadingCount = this.data.loadingCount - 1;
+    //     let col1 = this.data.col1;
+    //     let col2 = this.data.col2;
 
-        if (col1H <= col2H) {
-            col1H += imgHeight;
-            col1.push(imageObj);
-        } else {
-            col2H += imgHeight;
-            col2.push(imageObj);
-        }
+    //     if (col1H <= col2H) {
+    //         col1H += imgHeight;
+    //         col1.push(imageObj);
+    //     } else {
+    //         col2H += imgHeight;
+    //         col2.push(imageObj);
+    //     }
 
-        let data = {
-            loadingCount: loadingCount,
-            col1: col1,
-            col2: col2
-        };
+    //     let data = {
+    //         loadingCount: loadingCount,
+    //         col1: col1,
+    //         col2: col2
+    //     };
 
-        if (!loadingCount) {
-            data.images = [];
-        }
+    //     if (!loadingCount) {
+    //         data.images = [];
+    //     }
 
-        this.setData(data);
-    },
+    //     this.setData(data);
+    // },
     loadImages: function() {
         let that = this,
-            images = [],
+            images = that.data.images,
             param = new Object();
             param.offset = that.data.offset,
             app.net.$Api.getHomeList(param).then((res) => {
@@ -128,7 +128,8 @@ Page({
     onPullDownRefresh: function() {
         var that = this;
         that.setData({
-            pullDown: true
+            pullDown: true,
+            images:[]
         })
         wx.vibrateShort()
         wx.showNavigationBarLoading() //在标题栏中显示加载
