cc6633bb1ef69f1bd6902057cccaabfa01fbf116
17611658815 <17611658815@163.com>
2019-09-06 17:19:02 +0800

    2019-09-06

--- pages/cateDetaile/cateDetaile.js
+++ pages/cateDetaile/cateDetaile.js
@@ -12,7 +12,13 @@ Page({
             showCapsule: 1,
             title: '',
         },
-        detaileObj:{}
+        detaileObj:{},
+
+        writePosition: [80, 90],
+        writesize: [0, 0],// X Y 定位
+        window: [0, 0], //屏幕尺寸
+        write: [0, 0], //定位参数
+        scrolltop: 0,//据顶部距离
     },
 
     /**
@@ -24,7 +30,43 @@ Page({
             id: options.id,
             height: app.globalData.height,
         })
-        that.loadList()
+        that.loadList();
+        that.getSysdata();
+    },
+    //计算默认定位值
+    getSysdata: function () {
+        var that = this;
+        wx.getSystemInfo({
+            success: function (e) {
+                that.data.window = [e.windowWidth, e.windowHeight];
+                var write = [];
+                write[0] = that.data.window[0] * that.data.writePosition[0] / 100;
+                write[1] = that.data.window[1] * that.data.writePosition[1] / 100;
+                that.setData({
+                    write: write
+                }, function () {
+                    // 获取元素宽高
+                    wx.createSelectorQuery().select('.collectBox').boundingClientRect(function (res) {
+                        console.log(res.width)
+                        that.data.writesize = [res.width, res.height];
+                    }).exec();
+                })
+            },
+            fail: function (e) {
+                console.log(e)
+            }
+        });
+    },
+    //开始拖拽   
+    touchmove: function (e) {
+        var that = this;
+        var position = [e.touches[0].pageX - that.data.writesize[0] / 2, e.touches[0].pageY - that.data.writesize[1] / 2 - this.data.scrolltop];
+        that.setData({
+            write: position
+        });
+    },
+    onPageScroll(e) {
+        this.data.scrolltop = e.scrollTop;
     },
     loadList() {
         let that = this,
